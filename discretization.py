77df7f2b49cb1eeee7c1adc5a632faa227dfccaa
rkutri <r.kutri@gmail.com>
2020-02-23 22:48:06 +0100

    correct ambiguous semantics

--- discretization.py
+++ discretization.py
@@ -9,7 +9,7 @@ class FVDiscretization:
 
     Attributes
     ------
-    N_dof : integer
+    n_dof : integer
         The total number of cells used to subdivide the domain
     h : float
         The length of a single cell
@@ -19,24 +19,24 @@ class FVDiscretization:
         The dense load vector of the system
     """
 
-    def __init__(self, model_problem, N_dof):
+    def __init__(self, model_problem, n_dof):
         """
         Parameter
         ---------
         model_problem : ModelProblem.ModelProblem
             The model problem to be discretized
-        N_dof : integer
+        n_dof : integer
             The total number of cells used to subdivide the domain
         """
 
-        self.N_dof = N_dof
-        self.h = model_problem.domain_length / N_dof
+        self.n_dof = n_dof
+        self.h = model_problem.domain_length / n_dof
 
-        main_diag = np.full(N_dof, 1.0 + self.h)
-        off_diag = np.full(N_dof-1, -1.0)
+        main_diag = np.full(n_dof, 1.0 + self.h)
+        off_diag = np.full(n_dof-1, -1.0)
 
         # Use the csc format as this is later required for the solvers
         self.A = diags([main_diag, off_diag], [0, -1], format='csc')
 
-        self.b = np.full(N_dof, model_problem.s_eps * self.h)
+        self.b = np.full(n_dof, model_problem.s_eps * self.h)
         self.b[0] += model_problem.inflow_bc

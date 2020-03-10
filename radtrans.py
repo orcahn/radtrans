a8ef8d566b00efa84cce20771437081402d3db38
rkutri <r.kutri@gmail.com>
2020-03-10 15:44:38 +0100

    update formatting

--- radtrans.py
+++ radtrans.py
@@ -14,91 +14,116 @@ class RadiativeTransfer:
     Main class of the project.
     Reads in parameter .ini file, specifies model, discretization method
     and solver parameters and discretizes and solves the radiative transfer
-    problem for the given parameters. The numerical solution is then visualized.
+    problem for the given parameters. The numerical solution is then
+    visualized.
     """
 
     def main(self, argv):
+
         # parse parameter file
         config = configparser.ConfigParser()
         config.read(argv[1:])
-        dimension = int(config['Model']['Dimension'])
-        temperature = float(config['Model']['Temperature'])
-        frequency = float(config['Model']['Frequency'])
-        albedo = float(config['Model']['Albedo'])
-        scattering = str(config['Model']['Scattering'])
-        domain = config['Model']['Domain']
-        absorp = str(config['Model']['Absorption'])
-        absorption_coefficient = absorption.Absorption(float(domain))
-        if absorp == 'None':
-            absorption_coefficient = absorption_coefficient.no_abs
-        elif absorp == 'Const':
-            absorption_coefficient = absorption_coefficient.const_abs
-        elif absorp == 'PosGrad':
-            absorption_coefficient = absorption_coefficient.pos_grad_abs
-        elif absorp == 'Gaussian':
-            absorption_coefficient = absorption_coefficient.gaussian_abs
-        elif absorp == 'Step':
-            absorption_coefficient = absorption_coefficient.step_abs
+
+        dimension = int(config['MODEL']['dimension'])
+        temperature = float(config['MODEL']['temperature'])
+        frequency = float(config['MODEL']['frequency'])
+        albedo = float(config['MODEL']['albedo'])
+        domain = float(config['MODEL']['domain'])
+        abs_type = str(config['MODEL']['absorption_type'])
+
+        scattering = str(config['MODEL']['scattering'])
+        assert scattering in ['none', 'isotropic'], \
+            'Scattering type ' + scattering + ' currently not supported.'
+
+        absorption_coeff = absorption.Absorption(abs_type, domain)
+
         boundary_values = [
             float(
                 e.strip()) for e in config.get(
-                'Model',
-                'BoundaryValues').split(',')]
+                'MODEL',
+                'boundaryValues').split(',')]
+
         quadrature_weights = [
             float(
                 e.strip()) for e in config.get(
-                'Model',
-                'QuadratureWeights').split(',')]
-        self.method = str(config['Discretization']['Name'])
-        self.n_cells = int(config['Discretization']['n_cells'])
-        solver_name = str(config['Solver']['Name'])
-        preconditioner = str(config['Solver']['Preconditioner'])
-        initial_guess = str(config['Solver']['InitialGuess'])
+                'MODEL',
+                'quadratureWeights').split(',')]
+
+        self.method = str(config['DISCRETIZATION']['Name'])
+        self.n_cells = int(config['DISCRETIZATION']['n_cells'])
+
+        solver_name = str(config['SOLVER']['Name'])
+        initial_guess = str(config['SOLVER']['InitialGuess'])
+
+        preconditioner = str(config['SOLVER']['Preconditioner'])
+        assert preconditioner in ['none', 'LambdaIteration'], \
+            'Preconditioner ' + preconditioner + ' currently not supported.'
 
         # define model problem and discretization
-        domain = float(domain)  # 1d domain only has one length
         model_problem = modelProblem.ModelProblem1d(
-            temperature, frequency, albedo, scattering, absorption_coefficient, domain, boundary_values)
+            temperature, frequency, albedo, scattering,
+            absorption_coeff, domain, boundary_values)
+
         assert(self.method == 'FiniteVolume')
+
         if scattering == 'isotropic':
+
             self.disc = discretization.FiniteVolume1d(
                 model_problem, self.n_cells, quadrature_weights)
+
         else:
+
             self.disc = discretization.FiniteVolume1d(
                 model_problem, self.n_cells)
 
         # define stiffness matrix, load vector, solver and preconditioner
         if preconditioner == 'LambdaIteration':
             preconditioner = solver.LambdaPreconditioner(self.disc)
+
         A, b = self.disc.stiff_mat, self.disc.load_vec
 
         self.dom = np.arange(
             0.5 * self.disc.h, self.n_cells * self.disc.h, self.disc.h)
 
-        if initial_guess == "Inflow":
+        if initial_guess == "thermalEmission":
+
             x_in = np.full(self.disc.n_dof, model_problem.s_eps)
-        elif initial_guess == "Analytical":
+
+        elif initial_guess == "noScattering":
+
             sol1 = model_problem.inflow_bc[0] * \
                 np.exp(-self.dom) + model_problem.s_eps * \
                 (1 - np.exp(-self.dom))
+
             sol2 = model_problem.inflow_bc[1] * \
                 np.exp(-self.dom[::-1]) + model_problem.s_eps * \
                 (1 - np.exp(-self.dom[::-1]))
+
             x_in = np.concatenate((sol1, sol2), axis=0)
+
         else:
+
             x_in = None
+
         linear_solver = solver.Solver(solver_name, preconditioner)
+
         self.x, self.iters, self.elapsed_time = linear_solver.solve(A, b, x_in)
 
     def output_results(self):
+
         if self.method == "FiniteVolume":
+
             plt.step(self.dom, self.x[:self.n_cells])
+
         else:
+
             plt.plot(self.dom, self.x)
+
         plt.show()
 
 
 if __name__ == "__main__":
+
     radtrans = RadiativeTransfer()
     radtrans.main(sys.argv)
     radtrans.output_results()

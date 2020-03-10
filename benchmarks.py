d1576a616c5e1c0b5a804eef774249a5c84dfee9
Jonas Roller <orcahn@github.com>
2020-03-10 14:50:28 +0100

    Updated benchmarks to include variable absorption

--- benchmarks.py
+++ benchmarks.py
@@ -15,13 +15,14 @@ for precond in ['NoPreconditioning','LambdaIteration']:
     plt.subplot(2,2,k)
     for solver in ['GMRES','BiCGSTAB']:
         iters = []
-        for alb in np.linspace(0,1,10):
+        for alb in np.linspace(0,0.999999,10):
             config['Model'] = {'Dimension': '1',
                                'Temperature': '300',
                                'Frequency': '440e11',
                                'Albedo': alb,
                                'Scattering': 'isotropic',
                                'Domain': '5.0',
+                               'Absorption' : 'Step',
                                'BoundaryValues': '1.0,0.0',
                                'QuadratureWeights': '1.0,1.0'}
 
@@ -29,14 +30,14 @@ for precond in ['NoPreconditioning','LambdaIteration']:
                                         'n_cells': '100'}
             config['Solver'] = {'Name': solver,
                                 'Preconditioner': precond,
-                                'InitialGuess': 'Analytical'}
+                                'InitialGuess': 'Inflow'}
 
             with open('benchmarks.ini', 'w') as configfile:
                 config.write(configfile)
             radtrans.main(['file: ','benchmarks.ini'])
             iters.append(radtrans.iters) 
 
-        plt.title(r'1D, N=100, iso. scat., BVs: 1.0,0.0, '+precond)
+        plt.title(r'1D, N=100, iso. scat., BVs: 1.0,0.0, Step, '+precond)
         plt.plot(np.linspace(0,1,10),iters,label=solver)
         plt.xlabel(r'$\eta$')
         plt.ylabel('#iters')
@@ -57,6 +58,7 @@ for precond in ['NoPreconditioning','LambdaIteration']:
                                'Albedo': '0.5',
                                'Scattering': 'isotropic',
                                'Domain': '5.0',
+                               'Absorption' : 'Step',
                                'BoundaryValues': '1.0,0.0',
                                'QuadratureWeights': '1.0,1.0'}
 
@@ -64,14 +66,14 @@ for precond in ['NoPreconditioning','LambdaIteration']:
                                         'n_cells': n}
             config['Solver'] = {'Name': solver,
                                 'Preconditioner': precond,
-                                'InitialGuess': 'Analytical'}
+                                'InitialGuess': 'Inflow'}
 
             with open('benchmarks.ini', 'w') as configfile:
                 config.write(configfile)
             radtrans.main(['file: ','benchmarks.ini'])
             times.append(radtrans.elapsed_time) 
 
-        plt.title(r'1D, iso. scat., BVs: 1.0,0.0, '+precond)
+        plt.title(r'1D, iso. scat., BVs: 1.0,0.0, Step, '+precond)
         plt.plot(Ns,times,label=solver)
         plt.xlabel('n_cells')
         plt.ylabel('time (s)')

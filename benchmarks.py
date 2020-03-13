d8ee0df66002fce09f5b26b8d6f19156ce22cc6b
Jonas Roller <orcahn@github.com>
2020-03-13 19:59:23 +0100

    Intermediate commit

--- benchmarks.py
+++ benchmarks.py
@@ -10,46 +10,50 @@ radtrans = RadiativeTransfer()
 plt.figure()
 
 # iteration number benchmark
+
 k = 1
-for precond in ['NoPreconditioning', 'LambdaIteration']:
-    plt.subplot(2, 2, k)
+
+for precond in ['none', 'LambdaIteration']:
+    plt.subplot(1, 2, k)
     for solver in ['GMRES', 'BiCGSTAB']:
         iters = []
-        for alb in np.linspace(0, 0.999999, 10):
-            config['Model'] = {'Dimension': '1',
-                               'Temperature': '300',
-                               'Frequency': '440e11',
-                               'Albedo': alb,
-                               'Scattering': 'isotropic',
-                               'Domain': '5.0',
-                               'Absorption': 'Step',
-                               'BoundaryValues': '1.0,0.0',
-                               'QuadratureWeights': '1.0,1.0'}
+        for alb in np.linspace(0, 0.999999, 100):
+            config['MODEL'] = {'dimension': '1',
+                               'temperature': '300',
+                               'frequency': '440e11',
+                               'albedo': alb,
+                               'scattering': 'isotropic',
+                               'domain': '5.0',
+                               'absorptionType': 'step',
+                               'boundaryValues': '1.0,0.0',
+                               'quadratureWeights': '1.0,1.0'}
 
-            config['Discretization'] = {'Name': 'FiniteVolume',
-                                        'n_cells': '100'}
-            config['Solver'] = {'Name': solver,
+            config['DISCRETIZATION'] = {'method': 'finiteVolume',
+                                        'n_cells': '1000'}
+            config['SOLVER'] = {'solver': solver,
                                 'Preconditioner': precond,
-                                'InitialGuess': 'Inflow'}
+                                'initialGuess': 'thermalEmission'}
 
             with open('benchmarks.ini', 'w') as configfile:
                 config.write(configfile)
             radtrans.main(['file: ', 'benchmarks.ini'])
             iters.append(radtrans.iters)
 
-        plt.title(r'1D, N=100, iso. scat., BVs: 1.0,0.0, Step, '+precond)
-        plt.plot(np.linspace(0, 1, 10), iters, label=solver)
+        plt.title(r'1D, N=1000, iso. scat., BVs: 1.0,0.0, Step, '+precond)
+        plt.plot(np.linspace(0, 1, 100), iters, label=solver)
         plt.xlabel(r'$\eta$')
         plt.ylabel('#iters')
         plt.legend()
     k += 1
 
+"""
 # execution time benchmarks
-Ns = [10*x for x in [2**j for j in range(10)]]
+Ns = [10*x for x in [2**j for j in range(18)]]
 
-for precond in ['NoPreconditioning', 'LambdaIteration']:
+#for precond in ['NoPreconditioning', 'LambdaIteration']:
+for precond in ['LambdaIteration']:
     plt.subplot(2, 2, k)
-    for solver in ['GMRES', 'BiCGSTAB']:
+    for solver in ['SparseDirect','GMRES','BiCGSTAB']:
         times = []
         for n in Ns:
             config['Model'] = {'Dimension': '1',
@@ -79,5 +83,5 @@ for precond in ['NoPreconditioning', 'LambdaIteration']:
         plt.ylabel('time (s)')
         plt.legend()
     k += 1
-
+"""
 plt.show()
